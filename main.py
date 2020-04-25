a7dc019bb83cc9a2071d30aebaf2cf0712ad7736
gyanendraknojiya <gyanendrak064@gmail.com>
2020-04-25 14:54:14 +0530

    Init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import PyPDF2
+import sys
+
+source_file = PyPDF2.PdfFileReader(sys.argv[1], 'rb')
+
+watermark_file = PyPDF2.PdfFileReader(sys.argv[2], 'rb')
+output = PyPDF2.PdfFileWriter()
+number_page = source_file.getNumPages()
+for i in range(number_page):
+    page = source_file.getPage(i)
+    page.mergePage(watermark_file.getPage(0))
+    output.addPage(page)
+
+with open('wmfile.pdf', 'wb') as file:
+    output.write(file)
